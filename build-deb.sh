976e29a717a976cb81ea65ab6486f784a5f16706
Robert Fiser <robert.fiser@socialbakers.com>
2015-09-10 14:47:37 +0200

    new version of sbks-morphlines

--- build-deb.sh
+++ build-deb.sh
@@ -17,7 +17,7 @@
 #!/bin/bash -eu
 
 VERSION=1.7
-SBKS_VERSION=4
+SBKS_VERSION=7
 filename=hbase-indexer_$VERSION~sbks~$SBKS_VERSION
 if [ ! -z $( find -name "$filename"_* ) ]; then
 	echo "$filename already exists, please bump sbks version"
@@ -37,6 +37,7 @@ rc=$?; if [ $rc != 0 ]; then exit $rc; fi
 cd hbase-indexer-dist/target/
 
 mkdir -p opt/hbase-indexer
+echo "rm -f /opt/hbase-indexer/lib/*" > preinst
 # tar -xzf hbase-$VERSION-hadoop2-bin.tar.gz -C opt/hbase --strip=1
 cp -p -R hbase-indexer-*/hbase-indexer-*/* opt/hbase-indexer
 # rm -rf opt/hbase/docs
@@ -46,6 +47,6 @@ cp -p -R hbase-indexer-*/hbase-indexer-*/* opt/hbase-indexer
 #../../nxfetch.sh -i com.socialbakers.protobuf:sbks-protos:0.1-protobuf-2.5.0-SNAPSHOT > opt/hbase-indexer/lib/sbks-protos-0.1.jar
 #../../nxfetch.sh -i com.sematext:kite-morphlines-elasticsearch:0.1.1-SBKS-FORK > opt/hbase-indexer/lib/kite-morphlines-elasticsearch-0.1.1.jar
 
-fpm -s dir -t deb --config-files opt/hbase-indexer/conf/ -n hbase-indexer -v $VERSION~sbks~$SBKS_VERSION -a amd64 -C . opt/hbase-indexer
+fpm --before-install preinst -s dir -t deb --config-files opt/hbase-indexer/conf/ -n hbase-indexer -v $VERSION~sbks~$SBKS_VERSION -a amd64 -C . opt/hbase-indexer
 
 cleanup
